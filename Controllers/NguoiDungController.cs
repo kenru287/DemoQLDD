ef7db418be7e025f98852ba89b55abdbfe36d570
HNC Q.3 TPHCM <HNC Q.3 TPHCM@HANOICOMPUTER>
2021-07-04 17:21:45 +0700

    Add project files.

---
+++ Controllers/NguoiDungController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoQLDD.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+    }
+}
